66c05e95ebe57ca87c9dd1c3771c56f60b2e74e6
Johnny Rocket <0.snilcy@gmail.com>
2023-03-15 20:34:56 +0300

    update

--- after/queries/lua/injections.scm
+++ after/queries/lua/injections.scm
@@ -21,6 +21,19 @@
     (#injection_comment! @_comment )
   )
   .
+  (assignment_statement
+    (expression_list
+      value: (string content: _ @injections.language)
+    )
+  )
+)
+
+(
+  (comment content: _ @_comment
+    (#injection_comment? @_comment )
+    (#injection_comment! @_comment )
+  )
+  .
   (block
     (variable_declaration
       (assignment_statement
